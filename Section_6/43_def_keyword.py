3ed706e25ac8c5874b47b3aed9c770a00b8d89a1
Michael Walter <michael@MacBook-Pro.local>
2020-07-27 21:50:05 -0700

    Completed 42 & 43.

---
+++ Section_6/43_def_keyword.py
@@ -0,0 +1,14 @@
+
+'''
+    Def Keyword:
+        Is used to declare that what follows is a function,
+        snake Casing, aka underscore is the convention for
+        function names. this_name
+
+'''
+
+def name_of_function():
+    pass
+
+def add_function(num1,num2):
+    return num1+num2
