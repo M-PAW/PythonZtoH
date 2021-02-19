e5b773f9890ff4f42786bf8a6fbc0c26cc4277e3
Michael Walter <michael_patrick.walter@outlook.com>
2021-02-19 00:43:00 -0800

    Milestone project 1: Finished building out all of the logic for tic-tac-toe, everything is working properly and I integrated the system call to clear the terminal at correct times including on exit.

--- Milestone_Projects/project-1_tic_tac_toe.py
+++ Milestone_Projects/project-1_tic_tac_toe.py
@@ -1,24 +1,40 @@
-
+import os
 # Using the numpad to play tic-tac-toe
 
 def display_board(board):
+    print(f"   |   |  ")
     print(f" {board[0]} | {board[1]} | {board[2]}")
+    print(f"---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]}")
+    print(f"---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]}")
+    print(f"   |   |  ")
 
-def player_input():
-    playerInput = int(input("Where to place your mark? "))
+def clearTerminal():
+    os.system('clear')
+
+def player_input(mark):
+    print("\n")
+    playerInput = int(input(f"Player {mark}, make your mark: "))
     return playerInput
 
 def place_marker(board, marker, position):
-    print(type(board))
-    board[position] = marker
+    board[position-1] = marker
 
     return board
 
 def win_check(board, mark):
-    win1 = [mark,mark,mark,'','','','','','']
-    win2 = ['','','',mark,mark,mark,'','','']
-    win3 = ['','','','','','',mark,mark,mark]
-    win4 = ['X']
+   
+    return ((board[0] == mark and board[1] == mark and board[2] == mark) or # Horiz top
+           (board[3] == mark and board[4] == mark and board[5] == mark) or # Horiz middle
+           (board[6] == mark and board[7] == mark and board[8] == mark) or # Horiz bottom
+           (board[0] == mark and board[3] == mark and board[6] == mark) or # Vert left
+           (board[1] == mark and board[4] == mark and board[7] == mark) or # Vert mid
+           (board[2] == mark and board[5] == mark and board[8] == mark) or # Vert right
+           (board[0] == mark and board[4] == mark and board[8] == mark) or # Diag LtoR
+           (board[2] == mark and board[4] == mark and board[6] == mark)) # Diag RtoL
+           
+
 
 
 def playerTurn(turn):
@@ -29,7 +45,16 @@ def playerTurn(turn):
 
 
 def replay():
-    pass
+    print("\n")
+    playMore = input("Would you like to play again? ")
+
+    if playMore.lower() == "y":
+        return True
+    else:
+        return False
+
+def resetBoard():
+    return ['1','2','3','4','5','6','7','8','9']
 
 def ticTacToe():
     
@@ -37,19 +62,41 @@ def ticTacToe():
 
     gameOn = True
     turn = True
+    win = False
+    moves = 0
 
 
     while gameOn:
+
+        clearTerminal()
 
         display_board(board)
 
         playerMark = playerTurn(turn)
 
-        position = player_input()
+        position = player_input(playerMark)
 
         board = place_marker(board, playerMark, position)
 
-        turn = not turn
+        win = win_check(board, playerMark)
+        if win:
+            print("\n")
+            print(f"PLAYER {playerMark} WINS!")
+            gameOn = replay()
+            if gameOn:
+                board = resetBoard()
+                moves = 0
+        elif moves == 8:
+            print("\n")
+            print("IT'S A TIE!")
+            gameOn = replay()
+            if gameOn:
+                board = resetBoard();
+                moves = 0
+        else:
+            moves += 1
+            turn = not turn
+    clearTerminal()
 
     
 ticTacToe()
