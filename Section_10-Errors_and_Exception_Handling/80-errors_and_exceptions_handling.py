2c33ba3fbf02dc16dcfc406696c53dc62400aa14
Michael Walter <michael_patrick.walter@outlook.com>
2021-02-24 21:22:32 -0800

    Section-10: Errors and exceptions. Fully understanding and getting the hang of it, super userful.

--- Section_10-Errors_and_Exception_Handling/80-errors_and_exceptions_handling.py
+++ Section_10-Errors_and_Exception_Handling/80-errors_and_exceptions_handling.py
@@ -50,10 +50,10 @@ def ask_for_int():
             result = int(input("Please provide a number: "))
         except:
             print("Whoops! Thas is not a number")
-        '''
-            You can combine else with except to be able to execute additional
-            code if there are no errors.
-        '''
+        
+            #You can combine else with except to be able to execute additional
+            #code if there are no errors.
+        
         else:
             print("Yes, Thank you!")
             break
@@ -62,4 +62,53 @@ def ask_for_int():
             print("I will always run at the end!")
 
 
-ask_for_int()
+#ask_for_int()
+
+#### Homework
+
+## Problem 1
+def square(arr):
+    try:
+        for i in arr:
+            print(i**2)
+    except TypeError:
+        print("The contents of your array are not the correct data-type for the operation.")
+    except:
+        print("An unknown error has occured.")
+    finally:
+        print("Operation complete.")
+
+#square(['a','b','c'])
+
+## Problem 2
+def divideByZero(a=5,b=0):
+    print("Starting Operation...")
+    try:
+        z = a/b
+        print(z)
+    except ZeroDivisionError:
+        print("Division error: cannot divide by zero.")
+    except:
+        print("An unknown error has occured.")
+    finally:
+        print("Operation Complete.")
+
+#divideByZero(5,2)
+
+def ask():
+    print("Beginning Operation")
+    while True:
+        number = input("Enter a number to square: ")  
+        try:
+            if number.isdigit():
+                number = int(number)
+            print(number**2)
+            break
+        except TypeError:
+            print("Incorrect Data-Type Entered, Try Again.")
+        except:
+            print("An Unknow Error Has Occured.")
+        finally:
+            print("Operation Complete")
+
+ask()
